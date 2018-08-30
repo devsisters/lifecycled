77f422e5aef3d5b480d464bffdf306aa7f0e1c40
Donghwa Kim <donghwa@devsisters.com>
2018-08-31 02:51:56 +0900

    Fix file descriptor leak issue

--- spot.go
+++ spot.go
@@ -20,32 +20,34 @@ func pollSpotTermination() chan time.Time {
 	log.Debugf("Polling metadata service for spot termination notices")
 	go func() {
 		for range time.NewTicker(time.Second * 5).C {
-			res, err := http.Get(metadataURLTerminationTime)
-			if err != nil {
-				log.WithError(err).Info("Failed to query metadata service")
-				continue
-			}
-			defer res.Body.Close()
+			func() {
+				res, err := http.Get(metadataURLTerminationTime)
+				if err != nil {
+					log.WithError(err).Info("Failed to query metadata service")
+					return
+				}
+				defer res.Body.Close()
 
-			// will return 200 OK with termination notice
-			if res.StatusCode != http.StatusOK {
-				continue
-			}
+				// will return 200 OK with termination notice
+				if res.StatusCode != http.StatusOK {
+					return
+				}
 
-			body, err := ioutil.ReadAll(res.Body)
-			if err != nil {
-				log.WithError(err).Info("Failed to read response from metadata service")
-				continue
-			}
+				body, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					log.WithError(err).Info("Failed to read response from metadata service")
+					return
+				}
 
-			// if 200 OK, expect a body like 2015-01-05T18:02:00Z
-			t, err := time.Parse(terminationTimeFormat, string(body))
-			if err != nil {
-				log.WithError(err).Info("Failed to parse time in termination notice")
-				continue
-			}
+				// if 200 OK, expect a body like 2015-01-05T18:02:00Z
+				t, err := time.Parse(terminationTimeFormat, string(body))
+				if err != nil {
+					log.WithError(err).Info("Failed to parse time in termination notice")
+					return
+				}
 
-			ch <- t
+				ch <- t
+			}()
 		}
 	}()
 
